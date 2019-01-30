897da85c05b2838fee293c08f10c9a242dd35fbf
Rhonate <42813302+Rhonate@users.noreply.github.com>
2019-01-30 16:42:09 +0300

    TundaApp

--- app/src/main/java/com/example/rhona/tundaapp/AddProduct.java
+++ app/src/main/java/com/example/rhona/tundaapp/AddProduct.java
@@ -1,13 +1,57 @@
 package com.example.rhona.tundaapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.jar.Manifest;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class AddProduct extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    Button camera;
+    ImageView pic;
+    private static final int CAMERA_REQUEST = 1888;
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        pic = (ImageView) this.findViewById(R.id.camera);
+        camera = (Button) this.findViewById(R.id.takepic);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            pic.setImageBitmap(photo);
+        }
     }
 }
