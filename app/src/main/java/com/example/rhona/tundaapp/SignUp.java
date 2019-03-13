53459d07e0d510f7bd1dfeec3d38591a52e912b6
Rhonate <42813302+Rhonate@users.noreply.github.com>
2019-03-13 20:23:56 +0300

    TundaApp

--- app/src/main/java/com/example/rhona/tundaapp/SignUp.java
+++ app/src/main/java/com/example/rhona/tundaapp/SignUp.java
@@ -1,5 +1,7 @@
 package com.example.rhona.tundaapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ThemedSpinnerAdapter;
@@ -12,88 +14,50 @@ import android.widget.Toast;
 
 import java.io.IOException;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class SignUp extends AppCompatActivity {
 
-    Button submit;
-    private static EditText fnametext, lnametext, tele, location, Seller_email, Seller_password;
-    private static String fname, lname, phone, address, seller_email, seller_password;
+    Button next;
+    private static EditText fnametext, lnametext, tele, location;
+    private static String fname, lname, phone, address;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
         //name tele loaction edittext
         fnametext=(EditText)findViewById(R.id.fnametxt);
         lnametext=(EditText)findViewById(R.id.lnametxt);
         tele=(EditText)findViewById(R.id.teletxt);
         location=(EditText)findViewById(R.id.locationtxt);
-        Seller_email=(EditText)findViewById(R.id.email);
-        Seller_password=(EditText)findViewById(R.id.passwd);
 
         fnametext.addTextChangedListener(nextTextWatcher);
         lnametext.addTextChangedListener(nextTextWatcher);
         tele.addTextChangedListener(nextTextWatcher);
         location.addTextChangedListener(nextTextWatcher);
-        Seller_email.addTextChangedListener(nextTextWatcher);
-        Seller_password.addTextChangedListener(nextTextWatcher);
 
         //submit button
-        submit=(Button)findViewById(R.id.submitbtn);
-        submit.setOnClickListener(new View.OnClickListener() {
+        next=(Button)findViewById(R.id.signupnextbtn);
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String email = Seller_email.getText().toString();
+                    fname = fnametext.getText().toString();
+                    lname = lnametext.getText().toString();
+                    phone = tele.getText().toString();
+                    address = location.getText().toString();
 
-                String validemail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                    Intent back = new Intent(SignUp.this, SignUp2.class);
+                    back.putExtra("firstname", fname);
+                    back.putExtra("lastname", lname);
+                    back.putExtra("phone", phone);
+                    back.putExtra("address", address);
 
-//                if (email.matches(validemail)) {
-//
-//
-//                    Toast.makeText(SignUp2.this, "Successfully Submitted", Toast.LENGTH_LONG).show(); //a toast
-//
-//                    Intent back = new Intent(SignUp2.this, LoginPage.class);
-//                    startActivity(back);
+                    startActivity(back);
 //
 //                    finish();
-//                } else {
 //
-//                    Toast.makeText(getApplicationContext(), "Enter Valid Email", Toast.LENGTH_LONG).show();
-//                }
-
-                Call<ResponseBody> call = RetrofitClient
-                        .getmInstance()
-                        .getApi()
-                        .addSeller(fname, lname, phone, address, seller_email, seller_password);
-
-                call.enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
-
-                        try {
-                            String url = response.body().string();
-                            Toast.makeText(SignUp.this, url, Toast.LENGTH_LONG).show();
-//
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        Toast.makeText(SignUp.this, t.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
-
             }
         });
 
@@ -107,14 +71,12 @@ public class SignUp extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             fname = fnametext.getText().toString().trim();
             lname = lnametext.getText().toString().trim();
             phone = tele.getText().toString().trim();
             address = location.getText().toString().trim();
-            seller_email = Seller_email.getText().toString().trim();
-            seller_password = Seller_password.getText().toString().trim();
-
-            submit.setEnabled(!fname.isEmpty() && !lname.isEmpty() && !phone.isEmpty() && !address.isEmpty() && !seller_email.isEmpty() && !seller_password.isEmpty());
+            next.setEnabled(!fname.isEmpty() && !lname.isEmpty() && !phone.isEmpty() && !address.isEmpty());
         }
 
         @Override
--- app/src/main/java/com/example/rhona/tundaapp/SignUp2.java
+++ app/src/main/java/com/example/rhona/tundaapp/SignUp2.java
@@ -1,127 +1,103 @@
-//package com.example.rhona.tundaapp;
+package com.example.rhona.tundaapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class SignUp2 extends AppCompatActivity {
+
+    private static String seller_email, seller_password;
+    Button back,submit;
+    EditText Seller_email, Seller_password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up2);
+
+        //email and password
+        Seller_email=(EditText)findViewById(R.id.email);
+        Seller_password=(EditText)findViewById(R.id.passwd);
+
+        //back button
+        back=(Button)findViewById(R.id.backbtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(SignUp2.this, SignUp.class);
+                startActivity(back);
+
+                finish();
+            }
+        });
+
+        //submit button
+        submit=(Button)findViewById(R.id.submitbtn);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = Seller_email.getText().toString();
+
+                String validemail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+//                if (email.matches(validemail)) {
+
+                Intent intent = getIntent();
+                String fname = intent.getStringExtra("firstname");
+                String lname = intent.getStringExtra("lastname");
+                String phone = intent.getStringExtra("phone");
+                String address = intent.getStringExtra("address");
 //
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.text.Editable;
-//import android.text.TextWatcher;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.Toast;
+                    Toast.makeText(SignUp2.this, fname+"\n"+lname+"\n"+phone+"\n"+address+"\n"+seller_email+"\n"+seller_password, Toast.LENGTH_LONG).show(); //a toast
 //
-//import java.io.IOException;
+//                    Intent back = new Intent(SignUp2.this, LoginPage.class);
+//                    startActivity(back);
 //
-//import okhttp3.ResponseBody;
-//import retrofit2.Call;
-//import retrofit2.Callback;
-//import retrofit2.Response;
+//                    finish();
+//                } else {
 //
-//public class SignUp2 extends AppCompatActivity {
-////    String fname = SignUp.getFname();
-////    String lname = SignUp.getLname();
-////    String phone = SignUp.getTele();
-////    String address = SignUp.getLocation();
-//
-//    private static String seller_email, seller_password;
-//    Button back,submit;
-//    EditText Seller_email, Seller_password;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_sign_up2);
-//
-//        //email and password
-//        Seller_email=(EditText)findViewById(R.id.email);
-//        Seller_password=(EditText)findViewById(R.id.passwd);
-//
-//        //back button
-//        back=(Button)findViewById(R.id.backbtn);
-//        back.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent back = new Intent(SignUp2.this, SignUp.class);
-//                startActivity(back);
-//
-//                finish();
-//            }
-//        });
-//
-//        //submit button
-//        submit=(Button)findViewById(R.id.submitbtn);
-//        submit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                String email = Seller_email.getText().toString();
-//
-//                String validemail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-//
-////                if (email.matches(validemail)) {
-////
-////
-////                    Toast.makeText(SignUp2.this, "Successfully Submitted", Toast.LENGTH_LONG).show(); //a toast
-////
-////                    Intent back = new Intent(SignUp2.this, LoginPage.class);
-////                    startActivity(back);
-////
-////                    finish();
-////                } else {
-////
-////                    Toast.makeText(getApplicationContext(), "Enter Valid Email", Toast.LENGTH_LONG).show();
-////                }
-//
-//                Call<ResponseBody> call = RetrofitClient
-//                        .getmInstance()
-//                        .getApi()
-//                        .addSeller(fname, lname, phone, address, seller_email, seller_password);
-//
-//                call.enqueue(new Callback<ResponseBody>() {
-//                    @Override
-//                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//
-//                        String s = "Connected";
-//                        Toast.makeText(SignUp2.this, s, Toast.LENGTH_LONG).show();
-//
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                        Toast.makeText(SignUp2.this, t.getMessage(), Toast.LENGTH_LONG).show();
-//                    }
-//                });
-//
-//            }
-//        });
-//
-//        //Log.i(" ",fname);
-//
-//        Seller_email.addTextChangedListener(submitTextWatcher);
-//        Seller_password.addTextChangedListener(submitTextWatcher);
-//
-//
-//
-//    }
-//
-//    private TextWatcher submitTextWatcher = new TextWatcher() {
-//        @Override
-//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//        }
-//
-//        @Override
-//        public void onTextChanged(CharSequence s, int start, int before, int count) {
-//            seller_email = Seller_email.getText().toString().trim();
-//            seller_password = Seller_password.getText().toString().trim();
-//
-//            submit.setEnabled(!seller_email.isEmpty() && !seller_password.isEmpty());
-//        }
-//
-//        @Override
-//        public void afterTextChanged(Editable s) {
-//
-//        }
-//    };
-//
-//}
+//                    Toast.makeText(getApplicationContext(), "Enter Valid Email", Toast.LENGTH_LONG).show();
+//                }
+
+
+            }
+        });
+
+        //Log.i(" ",fname);
+
+        Seller_email.addTextChangedListener(submitTextWatcher);
+        Seller_password.addTextChangedListener(submitTextWatcher);
+}
+
+    private TextWatcher submitTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            seller_email = Seller_email.getText().toString().trim();
+            seller_password = Seller_password.getText().toString().trim();
+
+            submit.setEnabled(!seller_email.isEmpty() && !seller_password.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+}
+//https://www.thecrazyprogrammer.com/2016/12/pass-data-one-activity-another-in-android.html
